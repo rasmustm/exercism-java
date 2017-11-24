55ad1444ba798413a737566da5c7bc74be560a70
Rasmus Thorseth Molander <rtm8@st-andrews.ac.uk>
2017-11-24 10:39:23 +0000

    Second iteration of RnaTranscription task

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,24 +1,28 @@
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class RnaTranscription
 {
 
-  public static String transcribe(String dnaStrand)
+  private static String dnaBaseToRnaBase(char c)
   {
-    String rnaStrand = "";
-
-    Map<Character,Character> transcription =
-      new HashMap<Character,Character>(){{
-      put('G', 'C');
-      put('C', 'G');
-      put('T', 'A');
-      put('A', 'U');
+    Map<Character,String> transcription =
+      new HashMap<Character,String>(){{
+      put('G', "C");
+      put('C', "G");
+      put('T', "A");
+      put('A', "U");
     }};
 
-    for (char c : dnaStrand.toCharArray())
-      rnaStrand = rnaStrand + transcription.get(c);
+    return transcription.get(c);
+  }
 
-    return rnaStrand;
+  public static String transcribe(String dnaStrand)
+  {
+    return dnaStrand.chars()
+                    .mapToObj(c -> dnaBaseToRnaBase((char) c))
+                    .collect(Collectors.joining());
   }
 
 }
